0c348f7ee8eaabf654adc2b17eb13b4d01d3deb9
Piturong <s6812022610441@email.kmutnb.ac.th>
2025-07-24 15:56:14 +0700

    งานที่ Coding_w05

--- Coding_w05-01.c
+++ Coding_w05-01.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 int main() {
-    char name = 'p'; // char ประกาศตัวแปรชื่อ
-    int age = 19; // int ประกาศตัวแปรอายุ
-    float weight = 48.5; // float ประกาศตัวแปรน้ำหนัก
+    char name = 'p'; // char ประกาศตัวแปรชื่อ 
+    int age = 19; // int ประกาศตัวแปรอายุ 
+    float weight = 48.5; // float ประกาศตัวแปรน้ำหนัก จุดทศนิยม
 
     printf("Student %c is %d  years old.\n", name,age ); // %c แสดงชื่อและ %d อายุของนักเรียน
     printf("His weights is %.1f kg.\n", weight); // แสดงน้ำหนักของนักเรียน%.1f แสดงทศนิยม 1 ตำแหน่ง
---
+++ Coding_w05-02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    char First_Name[30] ; //Char ประกาศตัวแปรชื่อ
+    int age ; //Int ประกาศตัวแปรอายุ
+    float hight; //Float ประกาศตัวแปรส่วนสูง
+    char subject[10]; //Char ประกาศตัวแปรชื่อวิชา       
+    float grade; //Float ประกาศตัวแปรเกรด
+    char gradeA; //Char ประกาศตัวแปรสัญลักษณ์เกรด                                                       
+   
+    printf("Enter your First Name age and hight):\n"); //ใส่ชื่อ อายุ และส่วนสูง
+    scanf("%s %d %f", &First_Name,&age, &hight); //รับค่าชื่อ นามสกุล อายุ และส่วนสูง
+
+    printf("Enter your Subject, grade and symbolized:\n"); //ใส่วิชา เกรด และสัญลักษณ์เกรด
+    scanf("%s %f %c", &subject, &grade, &gradeA); //รับค่าชื่อวิชา เกรด และสัญลักษณ์เกรด
+
+    printf("%s is %d years old and %.1f centimeters tall\n", First_Name,age, hight); //แสดงผลชื่อ อายุ และส่วนสูง
+    printf("In subject %s, %s got %.2f which is symbolized as '%c' ",subject,First_Name, grade, gradeA); //แสดงผลวิชา เกรด และสัญลักษณ์เกรด
+
+    return 0;              
+}
