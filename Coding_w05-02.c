9cc5b64583251d6ead49bd05ed87099aadcb84e3
Piturong <s6812022610441@email.kmutnb.ac.th>
2025-07-25 12:39:53 +0700

    งานที่5

--- Coding_w05-02.c
+++ Coding_w05-02.c
@@ -7,14 +7,14 @@ int main() {
     float grade; //Float ประกาศตัวแปรเกรด
     char gradeA; //Char ประกาศตัวแปรสัญลักษณ์เกรด                                                       
    
-    printf("Enter your First Name age and hight):\n"); //ใส่ชื่อ อายุ และส่วนสูง
-    scanf("%c %d %f", &First_Name,&age, &hight); //รับค่าชื่อ นามสกุล อายุ และส่วนสูง
+    printf("Enter your (First Name age and hight):\n"); //ใส่ชื่อ อายุ และส่วนสูง
+    scanf("%s %d %f", &First_Name,&age, &hight); //รับค่าชื่อ นามสกุล อายุ และส่วนสูง
 
-    printf("Enter your Subject, grade and symbolized:\n"); //ใส่วิชา เกรด และสัญลักษณ์เกรด
+    printf("Enter your (Subject, grade and symbolized):\n"); //ใส่วิชา เกรด และสัญลักษณ์เกรด
     scanf("%s %f %c", &subject, &grade, &gradeA); //รับค่าชื่อวิชา เกรด และสัญลักษณ์เกรด
 
-    printf("%s is %d years old and %.1f centimeters tall\n", First_Name,age, hight); //แสดงผลชื่อ อายุ และส่วนสูง
-    printf("In subject %s, %s got %.2f which is symbolized as '%c'\n",subject,First_Name, grade, gradeA); //แสดงผลวิชา เกรด และสัญลักษณ์เกรด
+    printf("%s is %d years old and %.1f centimeters tall.\n", First_Name,age, hight); //แสดงผลชื่อ อายุ และส่วนสูง
+    printf("In subject %s, %s got %.2f which is symbolized as '%c'.\n",subject,First_Name, grade, gradeA); //แสดงผลวิชา เกรด และสัญลักษณ์เกรด
 
     return 0;              
 }
